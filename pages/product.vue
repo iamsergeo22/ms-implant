c6302739c06364d7c3af959bd6ee544532969054
ALUtech166 <s.tassiga@alustudent.com>
2023-05-03 20:31:51 +0200

    update

---
+++ pages/product.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-container>
+      <h1 class="tw-text-2xl tw-font-bold tw-mb-8">Human Implant</h1>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-card class="tw-shadow-md tw-rounded-lg">
+            <v-img
+              class="tw-rounded-t-lg"
+              src="/images/implant-core.jpg"
+              aspect-ratio="1"
+            ></v-img>
+            <v-card-title class="tw-font-bold tw-text-lg">
+              Implant Core
+            </v-card-title>
+            <v-card-text>
+              The implant core is the central component of the implant that
+              contains the electronics and power source.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card class="tw-shadow-md tw-rounded-lg">
+            <v-img
+              class="tw-rounded-t-lg"
+              src="/images/implant-antenna.jpg"
+              aspect-ratio="1"
+            ></v-img>
+            <v-card-title class="tw-font-bold tw-text-lg">
+              Implant Antenna
+            </v-card-title>
+            <v-card-text>
+              The implant antenna is the component that allows the implant to
+              communicate wirelessly with external devices.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card class="tw-shadow-md tw-rounded-lg">
+            <v-img
+              class="tw-rounded-t-lg"
+              src="/images/implant-coil.jpg"
+              aspect-ratio="1"
+            ></v-img>
+            <v-card-title class="tw-font-bold tw-text-lg">
+              Implant Coil
+            </v-card-title>
+            <v-card-text>
+              The implant coil is the component that receives power wirelessly
+              from an external device.
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
